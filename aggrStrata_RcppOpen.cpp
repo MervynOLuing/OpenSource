312df523f2aec212ee327126dfd7c845fbe8d43c
MervynOLuing <48838026+MervynOLuing@users.noreply.github.com>
2022-12-12 17:39:28 +0000

    Update aggrStrata_RcppOpen.cpp

--- aggrStrata_RcppOpen.cpp
+++ aggrStrata_RcppOpen.cpp
@@ -1,3 +1,10 @@
+// Translation of R code from: G. Barcaroli, M. Ballin, H. Odendaal, D. Pagliuca, E. Willighagen, D. Zardetto (2020). SamplingStrata:
+//   Optimal Stratification of Sampling Frames for Multipurpose Sampling Surveys. R package version 1.5-2 URL
+// https://CRAN.R-project.org/package=SamplingStrata
+//
+// Giulio Barcaroli (2014). SamplingStrata: An R Package for the Optimization of Stratified Sampling.
+// Journal of Statistical Software, 61(4), 1-24. URL http://www.jstatsoft.org/v61/i04/. doi// 10.18637/jss.v061.i04
+
 #include <RcppArmadillo.h>
 using namespace Rcpp;
 //using namespace sugar;
@@ -42,22 +49,22 @@ IntegerVector Rcpp_sortDelta(IntegerVector x) {
 double sumof2=0.0;
 // [[Rcpp::export]]
 NumericMatrix   aggrStrata_RcppOpen(DataFrame strata, int nvar, IntegerVector vett,
-                                     int censiti, int dominio) {
-  
+                                    int censiti, int dominio) {
+
   strata["vett"] = vett;
   IntegerVector  varloop= seq(1, nvar);
   NumericVector N = strata["N"];
   NumericVector COST = strata["COST"];
   NumericVector CN = N *COST;
   double TMtot;
-  
+
   IntegerVector STRATO = Rcpp_sortDelta(unique(vett));
   NumericMatrix x(vett.size(), nvar);
   NumericMatrix y(vett.size(),nvar);
   for (int i=0; i<nvar;i++) {
     y(_,i)=NumericVector(strata[(2+i)]);
   }
-  
+
   int numCols = y.ncol();
   int numRows = y.nrow();
   int numGroups = STRATO.size();
@@ -65,12 +72,12 @@ NumericMatrix   aggrStrata_RcppOpen(DataFrame strata, int nvar, IntegerVector ve
   NumericVector AvVec(numGroups);
   NumericMatrix AvMat(nvar, numGroups);
   NumericMatrix TM(vett.size(),nvar);
-  
+
   // double sumof2;
   NumericMatrix Centroids(numGroups,numCols);
   for(i = 0; i < (numCols); ++i)
   {
-    
+
     for(k = 0; k < numGroups; ++k)
     {
       n=0;
@@ -78,7 +85,7 @@ NumericMatrix   aggrStrata_RcppOpen(DataFrame strata, int nvar, IntegerVector ve
       sumof2=0.0;
       for(j = 0; j < numRows; ++j)
       {
-        
+
         if (vett[j]==STRATO[k])
         {
           TMtot= y(j,i) *N[j];
@@ -92,18 +99,18 @@ NumericMatrix   aggrStrata_RcppOpen(DataFrame strata, int nvar, IntegerVector ve
       Centroids(k,i)=average;
     }
   }
-  
-  
+
+
   for(i = 0; i < (numCols); ++i)
   {
-    
+
     for(k = 0; k < numGroups; ++k)
     {
       for(j = 0; j < numRows; ++j){
-        
+
         if (vett[j]==STRATO[k])
         {
-          
+
           TM(j,i)= y(j,i) *N[j];
         }
       }
@@ -111,9 +118,9 @@ NumericMatrix   aggrStrata_RcppOpen(DataFrame strata, int nvar, IntegerVector ve
       AvMat(i, k)=average;
     }
   }
-  
-  
-  
+
+
+
   // NumericMatrix Centroids(numGroups,numCols);
   //
   //
@@ -124,25 +131,25 @@ NumericMatrix   aggrStrata_RcppOpen(DataFrame strata, int nvar, IntegerVector ve
   //
   NumericVector SumSquares(numGroups);
   NumericVector Nh(numGroups);
-  
+
   NumericMatrix std_dev(vett.size(),nvar);
   for (int i=0; i<nvar;i++) {
     std_dev(_,i)=NumericVector(strata[((2+nvar)+i)]);
   }
-  
+
   // NumericMatrix SquareMat =transpose(AvMat);
-  
-  
+
+
   double  TVARdouble;
-  
-  
+
+
   NumericMatrix TMt(nvar,numGroups);
   NumericMatrix TVAR(vett.size(),nvar);
   //double sumof2=0.0;
   double  TMtdouble;
   for(i = 0; i < (nvar); ++i)
   {
-    
+
     for(k = 0; k < numGroups; ++k)
     {
       sumOf=0.0;
@@ -151,31 +158,31 @@ NumericMatrix   aggrStrata_RcppOpen(DataFrame strata, int nvar, IntegerVector ve
       //"TVAR1 <- strata$S1**2 * (strata$N - 1)"
       for(j = 0; j < numRows; ++j)
       {
-        
+
         if (vett[j]==STRATO[k])
         {
-          
+
           TVAR(j,i)= (pow(std_dev(j,i),2))*(N[j]-1);
           TVARdouble+= pow(std_dev(j,i),2);
           TMtdouble+= (y(j,i) *N[j]);
         }
       }
-      
+
       TMt(i,k) = TMtdouble;
     }
-    
-    
-    
-    
-    
+
+
+
+
+
   }
-  
+
   for(k = 0; k < numGroups; ++k)
   {
     sumof2=0.0;
     for(j = 0; j < numRows; ++j)
     {
-      
+
       if (vett[j]==STRATO[k])
       {
         sumof2+=N[j];
@@ -184,59 +191,59 @@ NumericMatrix   aggrStrata_RcppOpen(DataFrame strata, int nvar, IntegerVector ve
     }
     Nh[k]=sumof2;
   }
-  
+
   NumericMatrix diff(vett.size(),nvar);
   for(i = 0; i < (nvar); ++i)
   {
-    
+
     for(k = 0; k < numGroups; ++k)
     {
       //"TVAR1 <- strata$S1**2 * (strata$N - 1)"
       for(j = 0; j < numRows; ++j)
       {
-        
+
         if (vett[j]==STRATO[k])
         {
           //  "strwrk$diff2 <- strwrk$N * ((1/strwrk$N)*strwrk$TM2 - (1/strwrk$Nt)*strwrk$TM2t)**2"
           //2*((((1/2)*25)-(1/1028)*(53191)))^2
           diff(j,i)=N[j]*(pow((((1/N[j])*TM(j,i))-((1/Nh[k])*TMt(i,k))),2));
-          
-          
-          
+
+
+
         }
       }
     }
   }
-  
-  
-  
+
+
+
   //}
-  
-  
+
+
   NumericMatrix aggrDiff(nvar,numGroups);
   double diffT;
   for(i = 0; i < (nvar); ++i)
   {
-    
+
     for(k = 0; k < numGroups; ++k)
     {
-      
+
       diffT=0.0;
       //"TVAR1 <- strata$S1**2 * (strata$N - 1)"
       for(j = 0; j < numRows; ++j)
       {
-        
+
         if (vett[j]==STRATO[k])
         {
           diffT+= diff(j,i);
         }
-        
+
       }
       aggrDiff(i,k) =  diffT;
-      
+
     }
   }
-  
+
   NumericMatrix TVARt(nvar,numGroups);
   double TVARtdouble;
   for(i = 0; i < (nvar); ++i)
@@ -255,7 +262,7 @@ NumericMatrix   aggrStrata_RcppOpen(DataFrame strata, int nvar, IntegerVector ve
       TVARt(i,k) =  TVARtdouble;
     }
   }
-  
+
   NumericMatrix copyTVARt= TVARt;
   NumericMatrix S_Mat(nvar,numGroups);
   double Nhk;
@@ -276,54 +283,54 @@ NumericMatrix   aggrStrata_RcppOpen(DataFrame strata, int nvar, IntegerVector ve
       Tvar_plus_diff=(TVARt_ik+diff_ik);
       ////Rcout << Tvar_plus_diff << "\n";
       Var_ik=((1/Nhk)*(TVARt_ik+diff_ik));
-      
+
       //Var_Mat(i,k)=Var_ik;
       S_Mat(i,k)=sqrt(Var_ik);
     }
   }
-  
-  
+
+
   NumericVector CostAv(k);
   double CostSum;
   double count;
-  
+
   for(k = 0; k < numGroups; ++k)
   {
-    
+
     CostSum=0.0;
     count=0.0;
     //"TVAR1 <- strata$S1**2 * (strata$N - 1)"
     for(j = 0; j < numRows; ++j)
     {
-      
+
       if (vett[j]==STRATO[k])
       {
         CostSum+= COST[j];
         count+=1;
       }
-      
+
     }
     CostAv[k] =  CostSum/count;
-    
+
   }
   IntegerVector DOM1(numGroups);
   for(k = 0; k < numGroups; ++k)
   {
     DOM1[k]=dominio;
   }
-  
+
   IntegerVector CENS(numGroups);
   for(k = 0; k < numGroups; ++k)
   {
     CENS[k]=censiti;
   }
   // SquareMat(k,i) = sumOf/sumof2;
-  
+
   //names(aggrStrata(strata,nvar,a,0,1))
-  
+
   CharacterVector names((5+(2*nvar)));
   names[0]="STRATO";
-  
+
   names[(1+(2*nvar))]="N";
   names[(2+(2*nvar))]="DOM1";
   names[(3+(2*nvar))]="COST";
@@ -341,30 +348,30 @@ NumericMatrix   aggrStrata_RcppOpen(DataFrame strata, int nvar, IntegerVector ve
   strcor.column(2+(2*nvar))=DOM1;
   strcor.column(3+(2*nvar))=CostAv;
   strcor.column(4+(2*nvar))=CENS;
-  
+
   for(i = 0; i < (nvar); ++i)
   {
-    
+
     names[1+i]="M" + std::to_string(i+1);
     names[((nvar+1)+i)]="S" + std::to_string(i+1);
     strcor.column(1+i)=round(Centroids.column(i),4);
     strcor.column((nvar+1)+i)=round(S_Mat.row(i),4);
   }
-  
-  
-  
+
+
+
   //NumericMatrix T_AvMat=transpose(AvMat);
-  
+
   // DataFrame strcor = DataFrame::create( Named("STRATO") = STRATO,
   //                    // AvMat,S_Mat,
   //                    Named("N") =Nh ,
   //                    Named("DOM1")=DOM1,
   //                    Named("COST")=CostAv,
   //                    Named("CENS") =CENS);
-  
+
   //Rcpp::DataFrame result(tmp);
   //result.attr("names") = names;
   colnames(strcor) = names;
   return strcor;
-  
+
 }
